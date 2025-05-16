c79c15df10977750cc0a222b29103405722cd257
ethan <ethannguyen.uts@gmail.com>
2025-05-16 15:57:44 +1000

    2300-sucess-pairs-spells-and-potions

---
+++ 2300-success-pairs-spells-and-potions.ts
@@ -0,0 +1,35 @@
+function successfulPairs(
+  spells: number[],
+  potions: number[],
+  success: number
+): number[] {
+  const sortedPotions = potions.sort((x, y) => x - y);
+  const result: number[] = [];
+
+  for (let i = 0; i < spells.length; i++) {
+    const spell = spells[i];
+
+    let mid = -1;
+    let position = -1;
+    let found = false;
+    let left = 0;
+    let right = potions.length - 1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (spell * potions[mid] >= success) {
+        found = true;
+        right = mid - 1;
+        position = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    if (found) {
+      result.push(potions.length - position);
+    } else {
+      result.push(0);
+    }
+  }
+
+  return result;
+}
